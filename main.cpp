90767a7675459ac406ac13e20b36b054e322bf09
Maciej S <115628283+MACKOzKLANU@users.noreply.github.com>
2023-06-01 23:37:20 +0200

    correct main.cpp

--- main.cpp
+++ main.cpp
@@ -4,96 +4,44 @@
 
 using namespace std;
 
-class Item {
-public:
-    string name;
-    int dmg;
-
-    Item(string name, int dmg) : name(name), dmg(dmg) {}
-};
-
 class Gracz {
 public:
     string name;
     int dmg;
     int hp;
-    vector<Item> items;
+     
 
-    Gracz(string name, int dmg, int hp) : name(name), dmg(dmg), hp(hp) {}
+    Gracz(string name, int dmg, int hp) : name(name), dmg(dmg), hp(hp){}
+    
 
-    void AddItem(const Item& item) {
-        items.push_back(item);
-    }
 };
 
-class Teams {
-public:
-    vector<Gracz> Team1;
-    vector<Gracz> Team2;
-
-    void AddToTeam1(string name, int dmg, int hp) {
-        Team1.push_back(Gracz(name, dmg, hp));
-    }
-
-    void AddToTeam2(string name, int dmg, int hp) {
-        Team2.push_back(Gracz(name, dmg, hp));
-    }
-
-    void ShowTeam(int teamNumber) {
-        
-        vector<Gracz> team;
-        if (teamNumber == 1) {
-            cout << "===TEAM1=== \n";
-            team = Team1;
-        } else if (teamNumber == 2) {
-            cout << "===TEAM2=== \n";
-
-            team = Team2;
-        } else {
-            cout << "incorrect team number" << endl;
-            return;
-        }
-
-        for (int i = 0; i < team.size(); i++) {
-            const Gracz& player = team[i];
-            cout << "name: " << player.name << endl;
-            cout << "dmg: " << player.dmg << endl;
-            cout << "hp: " << player.hp << endl;
-
-            if (!player.items.empty()) {
-                cout << "Items:" << endl;
-                for (int j = 0; j < player.items.size(); j++) {
-                    const Item& item = player.items[j];
-                    cout << "  name: " << item.name << endl;
-                    cout << "  dmg: " << item.dmg << endl;
-                }
-            }
-            else{
-                cout << "  no items \n";
-            }
-        }
-
-    }
-};
 
 int main() {
-    Teams teams;
-
-    teams.AddToTeam1("Jan", 5, 100);
-    teams.AddToTeam1("Anna", 8, 200);
-    teams.AddToTeam1("Kamil", 3, 50);
-
-    teams.AddToTeam2("Kamil", 3, 50);
-    teams.AddToTeam2("Kamil", 3, 50);
-    teams.AddToTeam2("Kamil", 3, 50);
-
-    teams.Team1[0].AddItem(Item("Sword", 10));
-    teams.Team1[1].AddItem(Item("Axe", 12));
-    teams.Team2[0].AddItem(Item("Bow", 7));
-    teams.Team2[2].AddItem(Item("Wand", 5));
-
-    teams.ShowTeam(1);
-    teams.ShowTeam(2);
+    vector<Gracz> Team1;
+    vector<Gracz> Team2;
+    
+    
+    Team1.push_back(Gracz("Jan", 5, 100));
+    Team1.push_back(Gracz("Anna", 8, 200));
+    Team1.push_back(Gracz("Kamil", 3, 50));
+    
+    Team2.push_back(Gracz("Kamil", 3, 50));
+    Team2.push_back(Gracz("Kamil", 3, 50));
+    Team2.push_back(Gracz("Kamil", 3, 50));
+    
+        
+        for(int i = 0;i < Team1.size();i++){
+            cout << "name: " << Team1[i].name << endl;
+            cout << "dmg: " << Team1[i].dmg << endl;
+            cout << "hp: " <<Team1[i].hp << endl;
+    }
+        for(int i = 0;i < Team2.size();i++){
+            cout << "name: " << Team2[i].name << endl;
+            cout << "dmg: " << Team2[i].dmg << endl;
+            cout << "hp: " <<Team2[i].hp << endl;
+    }
+    
 
     return 0;
 }
